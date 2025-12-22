139231cd36de23ed50c77ab05b0ef6ae87754dd4
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-12-22 22:38:03 +0900

    feat: add docs app and color picker, index.html

---
+++ docs/src/wasmJsMain/kotlin/docs/DocsApp.kt
@@ -0,0 +1,91 @@
+/*
+ * Designed and developed by 2022 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package docs
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import docs.component.DocsSidebar
+import docs.component.ThemeColorPickerDialog
+import docs.navigation.DocsRoute
+import docs.screen.ApiAlphaSliderScreen
+import docs.screen.ApiAlphaTileScreen
+import docs.screen.ApiBrightnessSliderScreen
+import docs.screen.ApiControllerScreen
+import docs.screen.ApiHsvColorPickerScreen
+import docs.screen.ApiImageColorPickerScreen
+import docs.screen.GettingStartedScreen
+import docs.screen.HomeScreen
+import docs.screen.InstallationScreen
+import docs.screen.PlatformSupportScreen
+import docs.screen.PlaygroundScreen
+import docs.theme.DefaultPrimaryColor
+import docs.theme.DocsTheme
+
+@Composable
+fun DocsApp() {
+  var isDarkTheme by remember { mutableStateOf(true) }
+  var primaryColor by remember { mutableStateOf(DefaultPrimaryColor) }
+  var showColorPicker by remember { mutableStateOf(false) }
+  var currentRoute by remember { mutableStateOf<DocsRoute>(DocsRoute.Home) }
+
+  DocsTheme(darkTheme = isDarkTheme, primaryColor = primaryColor) {
+    Row(modifier = Modifier.fillMaxSize().background(DocsTheme.colors.background)) {
+      DocsSidebar(
+        currentRoute = currentRoute,
+        onNavigate = { currentRoute = it },
+        isDarkTheme = isDarkTheme,
+        onToggleTheme = { isDarkTheme = !isDarkTheme },
+        primaryColor = primaryColor,
+        onOpenColorPicker = { showColorPicker = true },
+      )
+
+      Box(modifier = Modifier.weight(1f).fillMaxSize()) {
+        when (currentRoute) {
+          DocsRoute.Home -> HomeScreen(onNavigate = { currentRoute = it })
+          DocsRoute.GettingStarted -> GettingStartedScreen()
+          DocsRoute.Installation -> InstallationScreen()
+          DocsRoute.PlatformSupport -> PlatformSupportScreen()
+          DocsRoute.ApiHsvColorPicker -> ApiHsvColorPickerScreen()
+          DocsRoute.ApiImageColorPicker -> ApiImageColorPickerScreen()
+          DocsRoute.ApiAlphaSlider -> ApiAlphaSliderScreen()
+          DocsRoute.ApiBrightnessSlider -> ApiBrightnessSliderScreen()
+          DocsRoute.ApiAlphaTile -> ApiAlphaTileScreen()
+          DocsRoute.ApiController -> ApiControllerScreen()
+          DocsRoute.Playground -> PlaygroundScreen()
+        }
+      }
+    }
+
+    if (showColorPicker) {
+      ThemeColorPickerDialog(
+        currentColor = primaryColor,
+        onColorChanged = { primaryColor = it },
+        onDismiss = { finalColor ->
+          primaryColor = finalColor
+          showColorPicker = false
+        },
+      )
+    }
+  }
+}
